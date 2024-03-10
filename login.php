6298aff5f977862725af92efda0a6ac847a184df
Your Name <you@example.com>
2024-03-10 14:31:55 +0530

    added pass to login file

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
         </div>
 
         <div class="mb-3">
-            <label for="password" class="form-label">Password:</label>
+            <label for="password" class="form-label">Password: 123</label>
             <input type="password" class="form-control" id="password" name="password" required>
         </div>
 
